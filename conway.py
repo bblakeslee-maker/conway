ad38393739a847caee4abc7615ee56e070d0211f
Bryan Blakeslee <bblakeslee4@gmail.com>
2024-09-21 15:22:45 -0400

    Comments and formatting.

--- conway.py
+++ conway.py
@@ -28,20 +28,25 @@ class Conway:
             p=(1 - self.__threshold, self.__threshold))
 
     def update_grid(self):
-        new_board = np.zeros(self.__world.shape, dtype=int)
+        new_world = np.zeros(self.__world.shape, dtype=int)
         # This wrap parameter assumes that the world is round
         neighbor_counts = sni.convolve(self.__world, self.__neighbor_filter, mode='wrap')
-        # A cell that is alive changes state to dead if its neighbors include 0 or 1 alive cells
-        dead_cell_1 = (self.__world == 1) & ((neighbor_counts == 0) | (neighbor_counts == 1))
+
         # A cell that is alive retains its alive state if exactly 2 or 3 neighbors are alive
         survivor = (self.__world == 1) & ((neighbor_counts == 2) | (neighbor_counts == 3))
-        # A cell that is alive changes state to dead if more than 3 neighbors are alive
-        dead_cell_2 = (self.__world == 1) & (neighbor_counts > 3)
         # A cell that is dead becomes alive if exactly 3 of its neighbors are alive
         new_cell = (self.__world == 0) & (neighbor_counts == 3)
-        new_board[survivor | new_cell] = 1
-        new_board[dead_cell_1 | dead_cell_2] = 0
-        self.__world = new_board
+        new_world[survivor | new_cell] = 1
+
+        # Technically, since the new world is already zeroed, these conditions aren't necessary.
+        # However, they are included for completeness.
+        # A cell that is alive changes state to dead if its neighbors include 0 or 1 alive cells
+        dead_cell_1 = (self.__world == 1) & ((neighbor_counts == 0) | (neighbor_counts == 1))
+        # A cell that is alive changes state to dead if more than 3 neighbors are alive
+        dead_cell_2 = (self.__world == 1) & (neighbor_counts > 3)
+        new_world[dead_cell_1 | dead_cell_2] = 0
+
+        self.__world = new_world
         time.sleep(self.__interval)
         self.__iteration_counter += 1
         if self.dead_world():
